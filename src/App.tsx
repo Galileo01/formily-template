a8dc32711e0a007be91d387200b224792894e92b
Galileo01 <1354541676@qq.com>
2023-07-14 14:28:05 +0800

    feat: 补充案例2

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import * as React from 'react';
+import * as React from 'react'
 
-import { createForm } from '@formily/core';
-import { FormProvider, createSchemaField } from '@formily/react';
-import { Button } from 'antd';
-import { NumberPicker, FormItem, Submit, Input, FormLayout, FormButtonGroup, Select } from '@formily/antd';
-import 'antd/dist/antd.compact.min.css';
-import '@formily/antd/esm/style.less';
+import { createForm } from '@formily/core'
+import { FormProvider, createSchemaField } from '@formily/react'
+import { Button } from 'antd'
+import { NumberPicker, FormItem, Submit, Input, FormLayout, FormButtonGroup, Select } from '@formily/antd'
+import 'antd/dist/antd.compact.min.css'
+import '@formily/antd/esm/style.less'
 
-import LandLine, { land_line_validator } from './land-line';
+import LandLine, { land_line_validator } from './land-line'
+import CustomArrayItems from './custom-array-items'
 
 const SchemaField = createSchemaField({
-  components: { FormLayout, FormItem, NumberPicker, Submit, Input, Select, LandLine },
-});
+  components: { FormLayout, FormItem, NumberPicker, Submit, Input, Select, LandLine, CustomArrayItems },
+})
 
 export default function App() {
-  const form = React.useMemo(() => createForm(), []);
+  const form = React.useMemo(() => createForm(), [])
 
   return (
     <FormProvider form={form}>
@@ -29,19 +30,39 @@ export default function App() {
               validator: land_line_validator,
             }}
           />
+          <SchemaField.Array name="array" title="用户列表" x-component="CustomArrayItems" x-decorator="FormItem">
+            <SchemaField.Object>
+              <SchemaField.String name="name" title="名字" x-component="Input" x-decorator="FormItem" />
+              <SchemaField.Number
+                name="age"
+                title="年龄"
+                x-component="NumberPicker"
+                x-decorator="FormItem"
+                x-component-props={{ addonAfter: '岁' }}
+              />
+              <SchemaField.Number
+                name="height"
+                title="身高"
+                x-component="NumberPicker"
+                x-decorator="FormItem"
+                x-component-props={{ addonAfter: 'cm' }}
+              />
+            </SchemaField.Object>
+          </SchemaField.Array>
         </SchemaField>
         <FormButtonGroup.FormItem>
           <Submit onSubmit={console.log}>提交</Submit>
           <Button
             onClick={() => {
               form.setState((state) => {
-                state.editable = !state.editable;
-              });
-            }}>
+                state.editable = !state.editable
+              })
+            }}
+          >
             切换阅读态
           </Button>
         </FormButtonGroup.FormItem>
       </FormLayout>
     </FormProvider>
-  );
+  )
 }
--- src/custom-array-items/index.tsx
+++ src/custom-array-items/index.tsx
@@ -1,64 +1,63 @@
-import * as React from 'react';
+/**
+ * 使用  useField formily 的模式 自定义组件
+ * 务必使用  observer 包裹
+ */
+import * as React from 'react'
 
-import { observer, useField, useFieldSchema, RecursionField } from '@formily/react';
-import { ArrayField } from '@formily/core';
-import { Button } from 'antd';
-import { PlusOutlined } from '@ant-design/icons';
-import { FormItem } from '@formily/antd';
-import cn from 'classnames';
+import { observer, useField, useFieldSchema, RecursionField } from '@formily/react'
+import { ArrayField } from '@formily/core'
+import { Button } from 'antd'
+import { PlusOutlined, DeleteOutlined } from '@ant-design/icons'
+import { FormItem } from '@formily/antd'
 
-import styles from './index.module.less';
+import './index.less'
 
 export type ArrayItemHeaderProps = {
-  value?: Array<any>;
-  headerTitle: string;
-  addText: string;
-};
+  headerTitle: string
+}
 
 const CustomArrayItems = observer((props: ArrayItemHeaderProps) => {
-  const { value, headerTitle, addText } = props;
-  const field = useField() as ArrayField;
-  const schema = useFieldSchema();
+  const { headerTitle } = props
+  const field = useField() as ArrayField
+  const schema = useFieldSchema()
   return (
-    <div className={cn(styles['custom-array-items'], 'custom-array-items')}>
-      <div
-        className={cn(
-          'flex items-center justify-between custom-array-items-header',
-          styles['custom-array-items-header']
-        )}>
+    <div className="custom-array-items">
+      <div className="custom-array-items-header">
         <FormItem label={headerTitle} asterisk />
         <Button
-          type="link"
+          type="primary"
           onClick={() => {
-            field.push({});
-          }}>
+            field.push({})
+          }}
+        >
           <PlusOutlined />
-          {addText}
         </Button>
       </div>
       <div>
-        {value?.map((item, index) => {
+        {field.value?.map((item, index) => {
           return (
             <div
               // eslint-disable-next-line react/no-array-index-key
               key={`custom-array-items-${index}`}
               style={{ marginBottom: 10 }}
-              className="arco-formily-array-items-item-inner w-ful">
+              className="antd-formily-array-items-item-inner"
+            >
               <span className="mr-2">{index + 1}</span>
               <RecursionField schema={schema.items} name={index} />
-              <div
-                onClick={() => {
-                  field.remove(index);
-                }}
-                className="text-[#979AA8] cursor-pointer ml-3 shrink-0">
-                删除
-              </div>
+              {field?.editable && (
+                <DeleteOutlined
+                  onClick={() => {
+                    field.remove(index)
+                  }}
+                  className="delete-action"
+                />
+              )}
             </div>
-          );
+          )
         })}
       </div>
     </div>
-  );
-});
+  )
+})
 
-export default CustomArrayItems;
+export default CustomArrayItems
